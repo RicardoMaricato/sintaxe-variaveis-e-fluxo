ac2fe8dc1a71766f72f82a340b78d2bdef5a18bf
Ricardo Maricato <ricardo.maricato@hotmail.com>
2020-06-12 17:08:40 -0300

    primeiro commit

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,17 @@
+public class TestaPontoFlutuante {
+
+    public static void  main(String[] args){
+        double salario;
+        salario = 2800.70;
+        System.out.println("meu salário é " + salario);
+
+        double divisao = 120 / 2;
+        System.out.println(divisao);
+
+        int outraDivisao = 5 / 2;
+        System.out.println(outraDivisao);
+
+        double novaTentativa = 5.0 / 2;
+        System.out.println(novaTentativa);
+    }
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,23 @@
+public class TestaVariaveis {
+
+    public static void main(String[] args){
+        System.out.println("ola novo teste");
+
+        int idade;
+        idade = 28;
+
+        System.out.println(idade);
+
+        idade = 30 + 10;
+
+        System.out.println(idade);
+
+        idade = (7 * 2) + 10;
+
+        System.out.println(idade);
+
+        System.out.println("a idade é " + idade + ", Parabéns!");
+
+
+    }
+}
